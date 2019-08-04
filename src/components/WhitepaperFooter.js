c1fc726f9a8dd196d1625f6de330fd6a996bde84
Edgar Aroutiounian <edgar.factorial@gmail.com>
2019-08-04 14:51:36 -0700

    CSS module on whitepaper footer, remove when have 600+ width

--- src/components/WhitepaperFooter.js
+++ src/components/WhitepaperFooter.js
@@ -1,50 +1,50 @@
 import React from 'react';
-import GlobalEmitter from '../utils/EventEmitter'
+import GlobalEmitter from '../utils/EventEmitter';
+import styles from './whitepaper-footer.module.scss';
 
 const WhitepaperFooter = class extends React.Component {
-    clickOnWhitePaper(e) {
-        const pos = e.target.getClientRects()[0];
-        const left = pos.left;
-        const top = pos.top;
-        GlobalEmitter.emit('open-sticky', {
-            style: {left: left, top: top},
-            isReversed: true,
-            type: 'whitepaper'
-        });
-    }
+  clickOnWhitePaper(e) {
+    const pos = e.target.getClientRects()[0];
+    const left = pos.left;
+    const top = pos.top;
+    GlobalEmitter.emit('open-sticky', {
+      style: { left: left, top: top },
+      isReversed: true,
+      type: 'whitepaper',
+    });
+  }
 
-    clickOnOnepager(e) {
-        const pos = e.target.getClientRects()[0];
-        const left = pos.left;
-        const top = pos.top;
+  clickOnOnepager(e) {
+    const pos = e.target.getClientRects()[0];
+    const left = pos.left;
+    const top = pos.top;
 
-        GlobalEmitter.emit('open-sticky', {
-            style: {left: left, top: top},
-            isReversed: true
-        });
-    }
+    GlobalEmitter.emit('open-sticky', {
+      style: { left: left, top: top },
+      isReversed: true,
+    });
+  }
 
   render() {
     return (
-      <div className="whitepaper-fixed-desktop">
-          <a
-              className="outline button"
-              data-g-event="hero"
-              data-g-label="Whitepaper"
-              data-g-action="clicked"
-              onClick={this.clickOnOnepager}
-              onTouchMove={this.clickOnOnepager}
-          >
-              <span>Onepager</span>
-          </a>
+      <div className={`whitepaper-fixed-desktop ${styles.whitePaperFooter}`}>
+        <a
+          className="outline button"
+          data-g-event="hero"
+          data-g-label="Whitepaper"
+          data-g-action="clicked"
+          onClick={this.clickOnOnepager}
+          onTouchMove={this.clickOnOnepager}>
+          <span>Onepager</span>
+        </a>
 
         <span>&nbsp;&nbsp;&nbsp;</span>
-          <a className="header__nav-item button outline"
-             onClick={this.clickOnWhitePaper}
-             onTouchMove={this.clickOnWhitePaper}
-          >
-              <span>Whitepaper</span>
-          </a>
+        <a
+          className="header__nav-item button outline"
+          onClick={this.clickOnWhitePaper}
+          onTouchMove={this.clickOnWhitePaper}>
+          <span>Whitepaper</span>
+        </a>
       </div>
     );
   }
