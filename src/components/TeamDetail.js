da44e4dfb013b0f2d5c746a9d53c14a26aa4ce7d
Edgar Aroutiounian <edgar.factorial@gmail.com>
2019-08-04 14:43:40 -0700

    Use CSS module for team-bio lowering of font-size

--- src/components/TeamDetail.js
+++ src/components/TeamDetail.js
@@ -1,37 +1,47 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { graphql, StaticQuery } from 'gatsby'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql, StaticQuery } from 'gatsby';
+
+import styles from './team-detail.module.scss';
 
 class TeamDetail extends React.Component {
+  propTypes = {
+    data: PropTypes.shape({
+      allMarkdownRemark: PropTypes.shape({
+        edges: PropTypes.array,
+      }),
+    }),
+  };
   render() {
-    const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
+    const { data } = this.props;
+    const { edges: posts } = data.allMarkdownRemark;
 
     return (
       <div className="bios">
         {posts &&
           posts.map(({ node: post }, index) => (
-           <div className="bio"  key={index}>
-            <img src={post.frontmatter.avatar} className="bio__image"
-              srcSet={post.frontmatter.avatar && post.frontmatter.avatar.toString().replace('.','_2x.') + " 2x, " + post.frontmatter.avatar + " 1x"}
-              alt={post.frontmatter.name + ", Bio Image"} />
-            <h3>{post.frontmatter.name}</h3>
-            {post.frontmatter.desc.map( item => (
-              <p>{item.text}</p>
-            ))}
-         </div>
+            <div className="bio" key={index}>
+              <img
+                src={post.frontmatter.avatar}
+                className="bio__image"
+                srcSet={
+                  post.frontmatter.avatar &&
+                  post.frontmatter.avatar.toString().replace('.', '_2x.') +
+                    ' 2x, ' +
+                    post.frontmatter.avatar +
+                    ' 1x'
+                }
+                alt={post.frontmatter.name + ', Bio Image'}
+              />
+              <h3>{post.frontmatter.name}</h3>
+              {post.frontmatter.desc.map(item => (
+                <p className={styles.bio}>{item.text}</p>
+              ))}
+            </div>
           ))}
       </div>
-    )
+    );
   }
-}
-
-TeamDetail.propTypes = {
-  data: PropTypes.shape({
-    allMarkdownRemark: PropTypes.shape({
-      edges: PropTypes.array,
-    }),
-  }),
 }
 
 export default () => (
@@ -65,4 +75,4 @@ export default () => (
     `}
     render={(data, count) => <TeamDetail data={data} count={count} />}
   />
-)
+);
