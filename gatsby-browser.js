927639d362eff5e75906265d639470b51fbe8855
Edgar Aroutiounian <edgar.factorial@gmail.com>
2019-08-11 13:33:45 -0700

    Pick out right state block to move to

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,11 +3,19 @@ const anchorate = require('anchorate').default;
 exports.onRouteUpdate = location => {
   if (location.location.state) {
     const { state } = location.location;
-    setTimeout(() => {
-      const elem = document.querySelector(`${state.to_block}`);
-      if (elem) {
-        elem.scrollIntoView();
-      }
-    }, 800);
+    switch (state.to_block) {
+      case '#investor__block':
+      case '#partner__block':
+      case '#harmony_team__block':
+      case '#roadmap__block':
+        setTimeout(() => {
+          const elem = document.querySelector(`${state.to_block}`);
+          if (elem) {
+            elem.scrollIntoView();
+          }
+        }, 800);
+      default:
+        return;
+    }
   }
 };
